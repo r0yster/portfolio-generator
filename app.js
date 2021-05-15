ba3b0bcca4dda36dad40a8b365e388f96598d70f
royster <rcantu786@gmail.com>
2021-05-15 13:19:18 -0500

    updated template

--- app.js
+++ app.js
@@ -2,14 +2,6 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const generatePage = require('./src/page-template.js');
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', pageHTML, err => {
-//  if (err) throw err;
-
-// console.log('Portfolio complete! Check out index.html to see the output!');
-// });
-
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -137,11 +129,11 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
-    // will be uncommented in lesson 4
-    // const pageHTML = generatePage(portfolioData);
-    // fs.writeFile('./index.html', pageHTML, err => {
-    //   if (err) throw new Error(err);
-    //   console.log('Page created! Check out index.html in this directory to see it!');
-    // });
+    const pageHTML = generatePage(portfolioData);
+
+    fs.writeFile('./index.html', pageHTML, err => {
+      if (err) throw new Error(err);
+
+      console.log('Page created! Check out index.html in this directory to see it!');
+    });
   });
